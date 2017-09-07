e981bb3ae95ef154577ad9019ab0f56194a47441
Brady Stephenson <brmstephenson@gmail.com>
2017-09-07 09:40:45 -0400

    Mentoring day one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Pokemon} from './shared/models/pokemon.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Pokemon';
+  favoritePokemon = 'Charmander';
+  pokemon: Pokemon[] = [
+    new Pokemon(1, 'Bulbasaur'),
+    new Pokemon(2, 'Ivysaur'),
+    new Pokemon(3, 'Venusaur'),
+    new Pokemon(4, 'Charmander'),
+    new Pokemon(5, 'Charmeleon'),
+    new Pokemon(6, 'Charizard'),
+    { id: 25, name: 'Pikachu', height: 4, weight: 40 }
+  ];
+
+  changePokemon(newValue) {
+    this.favoritePokemon = newValue;
+  }
+
+  isPikachu(): boolean {
+    return this.favoritePokemon.toLowerCase() === 'pikachu';
+  }
+
+  getFavoriteClass(pk: Pokemon): string {
+    if (pk.name.toLowerCase() === this.favoritePokemon.toLowerCase()) {
+      return 'yellow-background';
+    }
+  }
 }
